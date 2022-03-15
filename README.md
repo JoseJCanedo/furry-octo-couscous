abafbe209a040c0bb40dc6dedc3b0f71597a308f
JoseJCanedo <canedo_jose@columbusstate.edu>
2022-03-15 03:28:19 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # furry-octo-couscous
+
+React app with node back end. To run cd brownies then npm start or yarn start.
+
+Front end runs on port 3000 and back end runs on 5000
---
+++ brownies/src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    console.log("This only run once!");
+   }, []);
+ return <div className="App"></div>;
+}
+
+export default App;
