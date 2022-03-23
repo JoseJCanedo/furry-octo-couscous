3a16b5c9b6ca39e40c746ad544dfb7a7829d7ef3
fieldaxiom <Janice.H.Canedo@gmail.com>
2022-03-23 03:12:22 +0000

    Added jssoup crawler to server.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const recipesearch = require("./routes/find_recipe")
 const express = require("express");  
 const PORT = process.env.PORT || 5000;
 const server = express();
@@ -7,6 +8,12 @@ server.use(express.urlencoded({ extended: true }));
 
 server.get("/api/hello", (req, res) => {
     res.status(200).send("Hello World Bitch!");
+});
+
+server.get("/api/recipe", (req, res) => {
+    console.log("/api/recipe")
+    var details = recipesearch.searchRecipe("brownies", ["almond butter"], null, 'ra');
+    res.status(200).send(details);
 });
 
 server.listen(PORT, () => console.log(`listening on port ${PORT}`));
